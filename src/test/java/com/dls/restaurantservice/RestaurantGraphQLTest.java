7b8f3637aa846e11d290dfc147c294b149138c7a
Benjamin-Harris1 <beha1503@gmail.com>
2026-06-11 13:27:21 +0200

    refactor: update API endpoints to use plural nouns for consistency

--- src/test/java/com/dls/restaurantservice/RestaurantGraphQLTest.java
+++ src/test/java/com/dls/restaurantservice/RestaurantGraphQLTest.java
@@ -11,13 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @AutoConfigureGraphQlTester
 @ActiveProfiles("test")
-@WithMockUser
 public class RestaurantGraphQLTest {
 
     @Autowired
